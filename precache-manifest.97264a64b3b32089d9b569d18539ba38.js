6482d8e1e528ad1157b3da1602ccec38a8896ad7
Grant Shepert <grant.shepert@getmura.com>
2020-04-08 13:24:44 +0100

    Updates

---
+++ precache-manifest.97264a64b3b32089d9b569d18539ba38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1f9160c58b36ad7f6eaf24e31d4da56",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "3c07ad0180eb0a43769a",
+    "url": "/monsters/static/css/main.39f5100b.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c07ad0180eb0a43769a",
+    "url": "/monsters/static/js/main.90ccec61.chunk.js"
+  },
+  {
+    "revision": "e20b59329f3960f98de4",
+    "url": "/monsters/static/js/runtime-main.e2917106.js"
+  }
+]);
